fb6f313ddf3a5c7ad37e31472caa9452fd17f757
Richie Holsenback <richieholsenback@gmail.com>
2020-11-06 11:50:00 -0600

    event card improvements

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -5,14 +5,20 @@ import "./Event.css";
 
 export const EventCard = ({ event }) => (
     <Card className="eventCard">
-        <Image className="eventcardposter" src={event.poster} wrapped ui={false} />
-        <Card.Header className="event__title">
+        <Link href={`/events/detail/${event.id}`}>
+            <Image className="eventcardposter" src={event.poster} wrapped ui={false} width="100%"/>
+        </Link>
+        <Card.Content>
+        <Card.Header>
             <Link to={`/events/detail/${event.id}`}>
+                <h4 className="event__title">
                 {event.name}
+                </h4>
             </Link>
         </Card.Header>
-        <Card.Meta className="eventCardDetail">
+        <Card.Meta className="event__date">
             {event.date}
         </Card.Meta>
+        </Card.Content>
     </Card>
-    )
+)
